6be7f5cdf4c6e71bcb9255026f70e7af4825daad
Alexandrina Mehandzhiyska <73063309+alexmehandzhiyska@users.noreply.github.com>
2026-07-08 22:28:07 +0300

    feat: add register
    * feat(auth): add register page design
    
    * chore(register): fix wording

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 
 import Layout from './components/common/Layout/Layout'
 import Home from './components/Home/Home'
+import Register from './components/Register/Register'
 
 function App() {
     return (
         <Layout>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </Layout>
     )
